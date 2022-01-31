09f5dcc42345626443e71780742b9f671639eeb7
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-31 09:28:22 -0800

    commit tree 5246f9b4ceb6d5dc5bae417fdfc2546640d8138b

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function ConditionalP(props) {
+  if (props.bio) {
+    return <p>{props.bio}</p>;
+  } else {
+    return null;
+  }
+}
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <ConditionalP bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
